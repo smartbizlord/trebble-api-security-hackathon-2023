0d54098b0a2230d9a0b8d855544951d27e0293d3
smartbizlord <smartbizlord@gmail.com>
2023-07-05 17:47:33 +0100

    made accept headers compulsory

--- middlewares/headers.js
+++ middlewares/headers.js
@@ -1,3 +1,5 @@
+const httpStatus = require("http-status")
+
 const jsonHeader = (req, res, next) => {
     res.setHeader("Content-Type", "application/json")
     res.setHeader(`Accept`, "application/json")
@@ -5,7 +7,13 @@ const jsonHeader = (req, res, next) => {
     res.setHeader("X-Frame-Options", "deny")
     res.setHeader("Content-Security-Policy", "default-src 'self'; connect-src 'self'; font-src 'self'; frame-src 'self'; img-src 'self'; manifest-src 'self'; media-src 'self'; object-src 'self'; style-src 'self'; worker-src 'self'")
     res.setHeader("Strict-Transport-Security", "max-age=63072000")
-    next()
+    if(req.headers.accept != "application/json") {
+        return next(new ApiError(httpStatusus.BAD_REQUEST, {
+            message: "Accept Header is missing from the request"
+        }));
+    }else {
+        next()
+    }
 }
 
 const allowedMethod = (req, res, next) => {
